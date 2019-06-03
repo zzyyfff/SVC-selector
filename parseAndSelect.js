bfadc452cb91e014b4689cde7c83204e49cea889
zzyyfff <jonathan.marcus@gmail.com>
2019-06-03 00:18:40 -0700

    display using json stringify color

--- parseAndSelect.js
+++ parseAndSelect.js
@@ -1,16 +1,14 @@
 'use strict'
 
-const fs = require('fs')
-const util = require('util')
-util.inspect.styles.name = 'underline'
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
+const fs = require('fs')
 let rawdata = fs.readFileSync('SystemViewController.json')
 let sysViewController = JSON.parse(rawdata)
-
+require('node-json-color-stringify')
 const SEPARATOR = '===================================================================================='
 
 const prompt = () => {
@@ -19,19 +17,14 @@ const prompt = () => {
     let running = true
     selectorString = selectorString.trim()
     if (selectorString === '*') {
-      console.log(SEPARATOR)
-      console.log(util.inspect(sysViewController, false, null, true))
-      console.log(SEPARATOR)
-      console.log('Displaying complete view hierarchy')
-      console.log('')
+      displayAll()
     } else if (selectorString === 'exit') {
       running = false
       readline.close()
     } else if (selectorString[0] !== '') {
       const [selectors, keys] = parseSelectorString(selectorString)
       selectedViewsIntoArray(selectors, keys, sysViewController, outputArray)
-      console.log(`Selector string '${selectorString}' returned ${outputArray.length} reseults`)
-      displayJson(outputArray, 'className', selectorString)
+      displayViewsAsJson(outputArray, selectorString)
     }
     if (running) prompt()
   })
@@ -45,6 +38,7 @@ function selectedViewsIntoArray (selectors, keys, obj, arr) {
     if (obj[keys[0]] === selectors[0]) selectedViewsIntoArray(selectors.slice(1), keys.slice(1), obj, arr)
     if (Array.isArray(obj[keys[0]]) && obj[keys[0]].includes(selectors[0])) selectedViewsIntoArray(selectors.slice(1), keys.slice(1), obj, arr)
   }
+
   if (obj['subviews'] && obj['subviews'].length > 0) {
     obj['subviews'].forEach(element => selectedViewsIntoArray(selectors, keys, element, arr))
   }
@@ -54,27 +48,6 @@ function selectedViewsIntoArray (selectors, keys, obj, arr) {
     obj['contentView']['subviews'].forEach(element => selectedViewsIntoArray(selectors, keys, element, arr))
   }
   if (obj['control']) selectedViewsIntoArray(selectors, keys, obj['control'], arr)
-}
-
-function displayJson (outputArray, key, selector) {
-  readline.question(`Display each result as JSON? (Y/n) `, (answer) => {
-    if (answer === 'Y' ||
-        answer === 'y' ||
-        answer === 'Yes' ||
-        answer === 'yes') {
-      console.log(SEPARATOR)
-      outputArray.forEach(element => {
-        console.log(util.inspect(element, false, null, true))
-        console.log(SEPARATOR)
-      })
-      console.log(`>> Displayed ${outputArray.length} reseults for ${key} '${selector}'`)
-      console.log('')
-      readline.pause()
-      prompt()
-    }
-    readline.pause()
-    prompt()
-  })
 }
 
 function parseSelectorString (selectorString) {
@@ -93,6 +66,37 @@ function parseSelectorString (selectorString) {
   }
 
   return [selectors, keys]
+}
+
+function displayViewsAsJson (outputArray, selectorString) {
+  console.log(`Selector string '${selectorString}' returned ${outputArray.length} reseults`)
+
+  readline.question(`Display each result as JSON? (Y/n) `, (answer) => {
+    if (answer === 'Y' ||
+        answer === 'y' ||
+        answer === 'Yes' ||
+        answer === 'yes') {
+      console.log(SEPARATOR)
+      outputArray.forEach(element => {
+        console.log(JSON.colorStringify(element, null, 2))
+        console.log(SEPARATOR)
+      })
+      console.log(`>> Displayed ${outputArray.length} reseults for ${selectorString}'`)
+      console.log('')
+      readline.pause()
+      prompt()
+    }
+    readline.pause()
+    prompt()
+  })
+}
+
+function displayAll () {
+  console.log(SEPARATOR)
+  console.log(JSON.colorStringify(sysViewController, null, 2))
+  console.log(SEPARATOR)
+  console.log('Complete view hierarchy.')
+  console.log('')
 }
 
 prompt()
