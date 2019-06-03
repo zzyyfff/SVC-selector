e2aee749d42b650e93962213069185fa4c16a93f
zzyyfff <jonathan.marcus@gmail.com>
2019-06-02 21:31:41 -0700

    Implemented selector chains as decendants

--- parseAndSelect.js
+++ parseAndSelect.js
@@ -11,31 +11,49 @@ const readline = require('readline').createInterface({
 let rawdata = fs.readFileSync('SystemViewController.json')
 let sysViewController = JSON.parse(rawdata)
 
+const SEPARATOR = '===================================================================================='
+
 const prompt = () => {
   const outputArray = []
   readline.question(`SVC-selector> `, (selectorString) => {
     let running = true
     selectorString = selectorString.trim()
     if (selectorString === '*') {
+      console.log(SEPARATOR)
       console.log(util.inspect(sysViewController, false, null, true))
+      console.log(SEPARATOR)
+      console.log('Displaying complete view hierarchy')
+      console.log('')
     } else if (selectorString === 'exit') {
       running = false
       readline.close()
-    } else if (selectorString[0] === '.') {
-      viewsOfKeyIntoArray(selectorString.slice(1), 'classNames', sysViewController, outputArray)
-      console.log(`className '${selectorString}' returned ${outputArray.length} reseults`)
+    } else if (selectorString[0] !== '') {
+      const [selectors, keys] = parseSelectorString(selectorString)
+      selectedViewsIntoArray(selectors, keys, sysViewController, outputArray)
+      console.log(`Selector string '${selectorString}' returned ${outputArray.length} reseults`)
       displayJson(outputArray, 'className', selectorString)
-    } else if (selectorString[0] === '#') {
-      viewsOfKeyIntoArray(selectorString.slice(1), 'identifier', sysViewController, outputArray)
-      console.log(`identifier '${selectorString}' returned ${outputArray.length} reseults`)
-      displayJson(outputArray, 'identifier', selectorString)
-    } else if (selectorString !== '') {
-      viewsOfKeyIntoArray(selectorString, 'class', sysViewController, outputArray)
-      console.log(`class '${selectorString}' returned ${outputArray.length} reseults`)
-      displayJson(outputArray, 'class', selectorString)
     }
     if (running) prompt()
   })
+}
+
+function selectedViewsIntoArray (selectors, keys, obj, arr) {
+  if (selectors.length === 1) {
+    if (obj[keys[0]] === selectors[0] && !arr.includes(obj)) arr.push(obj)
+    if (Array.isArray(obj[keys[0]]) && obj[keys[0]].includes(selectors[0]) && !arr.includes(obj)) arr.push(obj)
+  } else {
+    if (obj[keys[0]] === selectors[0]) selectedViewsIntoArray(selectors.slice(1), keys.slice(1), obj, arr)
+    if (Array.isArray(obj[keys[0]]) && obj[keys[0]].includes(selectors[0])) selectedViewsIntoArray(selectors.slice(1), keys.slice(1), obj, arr)
+  }
+  if (obj['subviews'] && obj['subviews'].length > 0) {
+    obj['subviews'].forEach(element => selectedViewsIntoArray(selectors, keys, element, arr))
+  }
+  if (obj['contentView'] &&
+      obj['contentView']['subviews'] &&
+      obj['contentView']['subviews'].length > 0) {
+    obj['contentView']['subviews'].forEach(element => selectedViewsIntoArray(selectors, keys, element, arr))
+  }
+  if (obj['control']) selectedViewsIntoArray(selectors, keys, obj['control'], arr)
 }
 
 function displayJson (outputArray, key, selector) {
@@ -44,10 +62,10 @@ function displayJson (outputArray, key, selector) {
         answer === 'y' ||
         answer === 'Yes' ||
         answer === 'yes') {
-      console.log('====================================================================================')
+      console.log(SEPARATOR)
       outputArray.forEach(element => {
         console.log(util.inspect(element, false, null, true))
-        console.log('====================================================================================')
+        console.log(SEPARATOR)
       })
       console.log(`>> Displayed ${outputArray.length} reseults for ${key} '${selector}'`)
       console.log('')
@@ -59,18 +77,22 @@ function displayJson (outputArray, key, selector) {
   })
 }
 
-function viewsOfKeyIntoArray (selector, key, obj, arr) {
-  if (obj[key] === selector) arr.push(obj)
-  if (Array.isArray(obj[key]) && obj[key].includes(selector)) arr.push(obj)
-  if (obj['subviews'] && obj['subviews'].length > 0) {
-    obj['subviews'].forEach(element => viewsOfKeyIntoArray(selector, key, element, arr))
+function parseSelectorString (selectorString) {
+  const [selectors, keys] = [selectorString.split(' '), []]
+
+  for (let i = 0; i < selectors.length; i++) {
+    if (selectors[i][0] === '.') {
+      keys.push('classNames')
+      selectors[i] = selectors[i].slice(1)
+    } else if (selectors[i][0] === '#') {
+      keys.push('identifier')
+      selectors[i] = selectors[i].slice(1)
+    } else {
+      keys.push('class')
+    }
   }
-  if (obj['contentView'] &&
-      obj['contentView']['subviews'] &&
-      obj['contentView']['subviews'].length > 0) {
-    obj['contentView']['subviews'].forEach(element => viewsOfKeyIntoArray(selector, key, element, arr))
-  }
-  if (obj['control']) viewsOfKeyIntoArray(selector, key, obj['control'], arr)
+
+  return [selectors, keys]
 }
 
 prompt()
